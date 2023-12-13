4071fdf7836e120828a28a10389331d130bea5fd
ugurhmz <craxx67@gmail.com>
2023-12-13 10:07:26 +0300

    Book objects saved to  MySQL Database

--- src/main/java/example/MainTwo.java
+++ src/main/java/example/MainTwo.java
@@ -1,20 +1,34 @@
 package example;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import example.entity.Book;
+import example.repository.BookRepository;
+import example.repositoryImpl.BookRepositoryImpl;
+
+import java.util.Calendar;
 import java.util.Date;
 
 public class MainTwo {
     public static void main(String[] args) {
 
+    BookRepository bookRepository = new BookRepositoryImpl();
 
+    Book book1 = new Book(0,"Yüzüklerin Efendisi", 4500,
+            "lorem ipsum","J. R. R. Tolkien", dateFormatter(6,1,2017));
+    Book book2 = new Book(0,"Harry Potter", 2500,
+                "lorem ipsum","J. K. Rowling", dateFormatter(26,04,1994));
 
+    bookRepository.save(book1);
+    bookRepository.save(book2);
+
+    }
+
+    public static Date dateFormatter(int day, int month, int year) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.DAY_OF_MONTH, day);
+        calendar.set(Calendar.MONTH, month);
+        calendar.set(Calendar.YEAR, year);
+
+        return calendar.getTime();
     }
 
 }
--- src/main/java/example/repositoryImpl/BookRepositoryImpl.java
+++ src/main/java/example/repositoryImpl/BookRepositoryImpl.java
@@ -6,7 +6,6 @@ import example.repository.BookRepository;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityTransaction;
 
-
 public class BookRepositoryImpl implements BookRepository {
 
     EntityManager entityManager = jpaFactory.getEntityManager();
@@ -18,24 +17,57 @@ public class BookRepositoryImpl implements BookRepository {
             transaction.begin();
             entityManager.persist(book);
             transaction.commit();
-            System.out.println("Kayıt başarılı...");
+            System.out.println("Kitap kayıt başarılı...");
         } catch (Exception e) {
-            System.out.println("işlem başarısız " + e.getMessage());
+            if (transaction.isActive()) {
+                transaction.rollback();
+            }
+            System.out.println("Kitap kayıt işlemi başarısız. Hata: " + e.getMessage());
+            e.printStackTrace();
         }
     }
 
     @Override
     public void delete(Book book) {
-
+        try {
+            transaction.begin();
+            entityManager.remove(book);
+            transaction.commit();
+            System.out.println("Silme işlem başarıyla tamamlandı...");
+        } catch (Exception e) {
+            if (transaction.isActive()) {
+                transaction.rollback();
+            }
+            System.out.println("Silme işlem başarısız oldu. Hata: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 
     @Override
     public Book find(int id) {
+        Book thatBook = entityManager.find(Book.class, id);
+
+        if (thatBook != null) {
+            return thatBook;
+        }
         return null;
     }
 
     @Override
     public Book update(Book book) {
-        return null;
+        try {
+            transaction.begin();
+            Book updatedBook = entityManager.merge(book);
+            transaction.commit();
+            System.out.println("Kitap güncelleme işlemi başarıyla tamamlandı. Güncellenen Kitap: " + updatedBook);
+            return updatedBook;
+        } catch (Exception e) {
+            if (transaction.isActive()) {
+                transaction.rollback();
+            }
+            System.out.println("Kitap güncelleme işlemi başarısız oldu. Hata: " + e.getMessage());
+            e.printStackTrace();
+            return null;
+        }
     }
 }
